2f9cec70b814293acfc28b2197c2b5967e39ea54
SebastianBoehringer <sebastian33.boehringer@gmail.com>
2019-12-21 17:42:32 +0100

    Javadoc and packageinfo for the package config and its subpackages

--- src/main/java/edu/horb/dhbw/config/thymeleaf/ThymeleafConfig.java
+++ src/main/java/edu/horb/dhbw/config/thymeleaf/ThymeleafConfig.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2019 Sebastian Boehringer.
+ *  This file is part of the CodeGenerator.
+ *
+ *  CodeGenerator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ * CodeGenerator is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ * along with CodeGenerator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package edu.horb.dhbw.config.thymeleaf;
 
 import org.thymeleaf.TemplateEngine;
@@ -5,18 +22,32 @@ import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
-public class ThymeleafConfig {
+/**
+ * Utility class that encapsulates methods for the Thymeleaf templateengine.
+ */
+public final class ThymeleafConfig {
 
-    public static TemplateEngine templateEngine(String language) {
+    private ThymeleafConfig() {
+
+    }
+
+    /**
+     * @param language The name of the programming language the generator
+     *                 should generate code for
+     * @return {@link TemplateEngine} prepared with an
+     * {@link ITemplateResolver} for the given language
+     */
+    public static TemplateEngine templateEngine(final String language) {
 
         TemplateEngine templateEngine = new TemplateEngine();
         templateEngine.setTemplateResolver(textTemplateResolver(language));
         return templateEngine;
     }
 
-    private static ITemplateResolver textTemplateResolver(String language) {
+    private static ITemplateResolver textTemplateResolver(final String language) {
 
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        ClassLoaderTemplateResolver templateResolver =
+                new ClassLoaderTemplateResolver();
         templateResolver.setPrefix("/templates/");
         templateResolver.setSuffix(".txt");
         templateResolver.setTemplateMode(TemplateMode.TEXT);
