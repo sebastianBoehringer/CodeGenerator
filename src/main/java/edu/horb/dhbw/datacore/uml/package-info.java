435b58241ac53870d3307bff9b2cc27dda70e55b
SebastianBoehringer <sebastian33.boehringer@gmail.com>
2019-12-21 17:43:59 +0100

    Javadoc and packageinfo for the package datacore.uml

---
+++ src/main/java/edu/horb/dhbw/datacore/uml/package-info.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2019 Sebastian Boehringer.
+ *  This file is part of the CodeGenerator.
+ *
+ *  CodeGenerator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ * CodeGenerator is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ * along with CodeGenerator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * Classes representing the classes of UML with the (mostly) same names.
+ * <br/>
+ * Due to naming conflicts classes like Class are called
+ * {@link edu.horb.dhbw.datacore.uml.structuredclassifiers.UMLClass}.
+ * You may access the current UML specification at
+ * <a href="https://www.omg.org/spec/UML/">this location</a>.
+ * Be aware that this implementation conforms to version 2.5.1 of the
+ * specification.
+ *
+ * @author sebastianBoehringer
+ * @version 0.1
+ * @since 0.1
+ */
+package edu.horb.dhbw.datacore.uml;
