364564e19547ff9c0509c8eb98c0841316aef489
Unknown <sebastian33.boehringer@gmail.com>
2019-12-30 14:19:34 +0100

    :new: NotYetImplementedException
    Added a new type of exception to mark methods whose implementation is currently missing but will later be implemented

---
+++ src/main/java/edu/horb/dhbw/util/NotYetImplementedException.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2019 Sebastian Boehringer.
+ *  This file is part of the CodeGenerator.
+ *
+ *  CodeGenerator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ * CodeGenerator is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ * along with CodeGenerator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.horb.dhbw.util;
+
+
+import lombok.NonNull;
+
+public class NotYetImplementedException extends RuntimeException {
+    /**
+     * Default Constructor.
+     * Deprecated to make myself aware that the method throwing this
+     * exception still has to be implemented.
+     */
+    @Deprecated
+    public NotYetImplementedException() {
+
+        super("The method you called has yet to be implemented.");
+    }
+
+    /**
+     * Deprecated to make myself aware that the method throwing this
+     * exception still has to be implemented.
+     *
+     * @param msg The msg for the exception
+     */
+    @Deprecated
+    public NotYetImplementedException(final String msg) {
+
+        super(msg);
+    }
+
+    /**
+     * Deprecated to make myself aware that the method throwing this
+     * exception still has to be implemented.
+     *
+     * @param clazz      The class in which the exception is thrown
+     * @param methodName The name of the method that still needs to be
+     *                   implemented
+     */
+    @Deprecated
+    public NotYetImplementedException(
+            @NonNull final Class clazz, @NonNull final String methodName) {
+
+        super(String.format(
+                "The method %s in class %s has yet to be " + "implemented",
+                methodName, clazz.getSimpleName()));
+    }
+}
