63e22beeb9e5485da8b44ff64f6064cd19a26503
Unknown <sebastian33.boehringer@gmail.com>
2020-01-03 19:51:54 +0100

    :white_check_mark: Added tests for restructuring
    Added tests for Property and UMLClassRestructurer

---
+++ src/test/java/edu/horb/dhbw/restructure/classes/PropertyRestructurerTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2020 Sebastian Boehringer.
+ *  This file is part of the CodeGenerator.
+ *
+ *  CodeGenerator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ * CodeGenerator is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ * along with CodeGenerator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.horb.dhbw.restructure.classes;
+
+import com.sdmetrics.model.Model;
+import edu.horb.dhbw.config.SDMetrics.SDMetricsConfig;
+import edu.horb.dhbw.datacore.uml.classification.Property;
+import edu.horb.dhbw.datacore.uml.enums.AggregationKind;
+import edu.horb.dhbw.datacore.uml.enums.VisibilityKind;
+import edu.horb.dhbw.restructure.IRestructurer;
+import edu.horb.dhbw.restructure.IRestructurerFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+import static org.testng.Assert.assertEquals;
+
+public class PropertyRestructurerTest {
+
+    private IRestructurerFactory factory =
+            new IRestructurerFactory(new HashMap<>());
+
+    @BeforeClass
+    public void init() {
+
+        factory.register(Property.class, new PropertyRestructurer(factory));
+    }
+
+    @Test(dataProvider = "classdiagramfiles")
+    public void onlyClassRestructuring(final String pathToFile) {
+
+        System.out.println(String.format("Testing file %s", pathToFile));
+        Model model;
+        try {
+            model = SDMetricsConfig.parseXMI(pathToFile);
+        } catch (Exception e) {
+            Assert.fail("Encountered an exception while processing xmi", e);
+            return;
+        }
+
+        IRestructurer<Property> restructurer =
+                factory.getIRestructurer(Property.class);
+        Collection<Property> properties = restructurer.restructure(model);
+        properties = properties.stream()
+                .filter(e -> !e.getName().equalsIgnoreCase("exporterversion"))
+                .collect(Collectors.toList());
+
+        assertEquals(properties.size(), 1, "There should only be one property");
+
+        for (Property property : properties) {
+            assertEquals(property.getName(), "member",
+                         "The name of the property should be member");
+            assertEquals(property.getVisibility(), VisibilityKind.PUBLIC,
+                         "Visibility should be public");
+            //TODO test type
+            //TODO test multiplicity bounds
+            assertEquals(property.getIsUnique(), Boolean.TRUE,
+                         "Property should be unique, i. e. this is true");
+            assertEquals(property.getIsOrdered(), Boolean.FALSE,
+                         "Default is false");
+            assertEquals(property.getAggregation(), AggregationKind.NONE,
+                         "Default is none");
+            assertEquals(property.getIsDerived(), Boolean.FALSE,
+                         "Default is false");
+            assertEquals(property.getIsComposite(), Boolean.FALSE,
+                         "Default is false");
+            assertEquals(property.getIsDerivedUnion(), Boolean.FALSE,
+                         "Default is false");
+            assertEquals(property.getIsID(), Boolean.FALSE, "Default is false");
+            assertEquals(property.getIsReadOnly(), Boolean.FALSE,
+                         "Default is false");
+            assertEquals(property.getIsStatic(), Boolean.FALSE,
+                         "Default is false");
+        }
+    }
+
+    @DataProvider(name = "classdiagramfiles")
+    public Object[][] provideClasses() {
+
+        return new Object[][]{
+                {"src/test/resources/classdiagrams/Classes_Mod.xmi"},
+                {"src/test/resources/classdiagrams/Classes_Pap.xmi"}};
+    }
+}
---
+++ src/test/java/edu/horb/dhbw/restructure/classes/UMLClassRestructurerTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2020 Sebastian Boehringer.
+ *  This file is part of the CodeGenerator.
+ *
+ *  CodeGenerator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ * CodeGenerator is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ * along with CodeGenerator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.horb.dhbw.restructure.classes;
+
+import com.sdmetrics.model.Model;
+import edu.horb.dhbw.config.SDMetrics.SDMetricsConfig;
+import edu.horb.dhbw.datacore.uml.enums.VisibilityKind;
+import edu.horb.dhbw.datacore.uml.structuredclassifiers.UMLClass;
+import edu.horb.dhbw.restructure.IRestructurer;
+import edu.horb.dhbw.restructure.IRestructurerFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import static org.testng.Assert.assertEquals;
+
+public class UMLClassRestructurerTest {
+
+    private IRestructurerFactory factory =
+            new IRestructurerFactory(new HashMap<>());
+
+    @BeforeClass
+    public void init() {
+
+        factory.register(UMLClass.class, new UMLClassRestructurer(factory));
+    }
+
+    @Test(dataProvider = "classdiagramfiles")
+    public void onlyClassRestructuring(final String pathToFile) {
+        System.out.println(String.format("Testing file %s", pathToFile));
+        Model model;
+        try {
+            model = SDMetricsConfig.parseXMI(pathToFile);
+        } catch (Exception e) {
+            Assert.fail("Encountered an exception while processing xmi", e);
+            return;
+        }
+
+        IRestructurer<UMLClass> restructurer =
+                factory.getIRestructurer(UMLClass.class);
+        Collection<UMLClass> classes = restructurer.restructure(model);
+        assertEquals(classes.size(), 1,
+                     "There should have been exactly one class");
+
+        for (UMLClass aClass : classes) {
+            assertEquals(aClass.getName(), "Class",
+                         "The name of the class should have been [Class]");
+            assertEquals(aClass.getVisibility(), VisibilityKind.PUBLIC,
+                         "Default visibility is public");
+            assertEquals(aClass.getIsAbstract(), Boolean.FALSE,
+                         "Default is false");
+            assertEquals(aClass.getIsActive(), Boolean.FALSE,
+                         "Default is false");
+            assertEquals(aClass.getIsFinalSpecialization(), Boolean.FALSE,
+                         "Default is false");
+        }
+
+    }
+
+    @DataProvider(name = "classdiagramfiles")
+    public Object[][] provideClasses() {
+
+        return new Object[][]{
+                {"src/test/resources/classdiagrams/Classes_Mod.xmi"},
+                {"src/test/resources/classdiagrams/Classes_Pap.xmi"}};
+    }
+}
