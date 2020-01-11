0e58f4042f7e3855636f0c1ec9f043265198e803
Unknown <sebastian33.boehringer@gmail.com>
2020-01-11 13:37:52 +0100

    :construction: restructuring for behavioredClassifiers

---
+++ src/main/java/edu/horb/dhbw/restructure/delegating/BehavioredClassifierRestructurer.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020 Sebastian Boehringer.
+ *  This file is part of the CodeGenerator.
+ *
+ *  CodeGenerator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ * CodeGenerator is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ * along with CodeGenerator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.horb.dhbw.restructure.delegating;
+
+import com.sdmetrics.model.ModelElement;
+import edu.horb.dhbw.datacore.uml.simpleclassifiers.BehavioredClassifier;
+import edu.horb.dhbw.restructure.IRestructurer;
+import edu.horb.dhbw.restructure.IRestructurerMediator;
+import edu.horb.dhbw.util.LookupUtil;
+import edu.horb.dhbw.util.XMIUtil;
+import lombok.NonNull;
+
+public final class BehavioredClassifierRestructurer
+        extends DelegatingRestructurer<BehavioredClassifier> {
+    /**
+     * @param iRestructurerMediator The mediator responsible for providing
+     *                              the other {@link IRestructurer}s
+     */
+    public BehavioredClassifierRestructurer(final IRestructurerMediator iRestructurerMediator) {
+
+        super(iRestructurerMediator, "behavioredclassifier");
+    }
+
+    @Override
+    public BehavioredClassifier restructure(
+            @NonNull final ModelElement element) {
+
+        Class<? extends BehavioredClassifier> clazz = LookupUtil
+                .behavioredClassifierFromUMLType(XMIUtil.getUMLType(element));
+        return delegateRestructuring(element, clazz);
+    }
+}
