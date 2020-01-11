567da4d3da7210d90264fc19a24fcf33d8fae486
Unknown <sebastian33.boehringer@gmail.com>
2020-01-11 12:09:21 +0100

    :construction: restructuring for directedRelationships

---
+++ src/main/java/edu/horb/dhbw/restructure/delegating/DirectedRelationshipRestructurer.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2020 Sebastian Boehringer.
+ *  This file is part of the CodeGenerator.
+ *
+ *  CodeGenerator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ * CodeGenerator is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ * along with CodeGenerator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.horb.dhbw.restructure.delegating;
+
+import com.sdmetrics.model.ModelElement;
+import edu.horb.dhbw.datacore.uml.commonstructure.DirectedRelationship;
+import edu.horb.dhbw.restructure.IRestructurer;
+import edu.horb.dhbw.restructure.IRestructurerMediator;
+import edu.horb.dhbw.util.LookupUtil;
+import edu.horb.dhbw.util.XMIUtil;
+import lombok.NonNull;
+
+public final class DirectedRelationshipRestructurer
+        extends DelegatingRestructurer<DirectedRelationship> {
+    /**
+     * @param iRestructurerMediator The mediator responsible for providing
+     *                              the other {@link IRestructurer}s
+     */
+    public DirectedRelationshipRestructurer(final IRestructurerMediator iRestructurerMediator) {
+
+        super(iRestructurerMediator, "directedrelationship");
+    }
+
+    @Override
+    public DirectedRelationship restructure(
+            @NonNull final ModelElement element) {
+
+        Class<? extends DirectedRelationship> clazz = LookupUtil
+                .directedRelationshipFromUMLType(XMIUtil.getUMLType(element));
+        return delegateRestructuring(element, clazz);
+    }
+
+}
