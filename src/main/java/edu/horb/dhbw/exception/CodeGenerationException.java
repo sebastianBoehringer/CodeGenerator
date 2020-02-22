ed65dbbf543d46cfa7e20af922691557db11ee6f
Unknown <sebastian33.boehringer@gmail.com>
2020-02-22 15:57:30 +0100

    :new: New kinds of Exceptions

---
+++ src/main/java/edu/horb/dhbw/exception/CodeGenerationException.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020 Sebastian Boehringer.
+ *  This file is part of the CodeGenerator.
+ *
+ *  CodeGenerator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ * CodeGenerator is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ * along with CodeGenerator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.horb.dhbw.exception;
+
+/**
+ * Signals that the generation of code failed.
+ */
+public class CodeGenerationException extends Exception {
+    public CodeGenerationException() {
+
+        super();
+    }
+
+    public CodeGenerationException(final String msg) {
+
+        super(msg);
+    }
+
+    public CodeGenerationException(final Throwable cause) {
+
+        super("Codegeneration failed, nested exception is " + cause.getClass()
+                .getSimpleName() + ", message: " + cause.getMessage());
+    }
+}
