37136470b92d82762377f0d3356b50d8915faeb4
Unknown <sebastian33.boehringer@gmail.com>
2020-02-22 16:01:46 +0100

    :new: Added CodeGenerator
    CodeGenerator class is supposed to be a facade hiding away the internal xmi and template processing

---
+++ src/main/java/edu/horb/dhbw/CodeGenerator.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2020 Sebastian Boehringer.
+ *  This file is part of the CodeGenerator.
+ *
+ *  CodeGenerator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ * CodeGenerator is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ * along with CodeGenerator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.horb.dhbw;
+
+import com.sdmetrics.model.Model;
+import edu.horb.dhbw.datacore.model.Language;
+import edu.horb.dhbw.datacore.uml.CommonElements;
+import edu.horb.dhbw.exception.CodeGenerationException;
+import edu.horb.dhbw.exception.InvalidConfigurationException;
+import edu.horb.dhbw.restructure.IRestructurerMediator;
+import edu.horb.dhbw.templating.ITemplateEngineAdapter;
+import edu.horb.dhbw.util.Config;
+import edu.horb.dhbw.util.SDMetricsUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Main class orchestrating the work of the different components.
+ * Said components are the XMI-parsing, the validation of the input, the
+ * transformation of the input into something useable and finally the
+ * generation of source code by using a template engine.
+ */
+@Slf4j
+public final class CodeGenerator {
+    /**
+     * The adapter that is configured for the codegenerator.
+     */
+    private ITemplateEngineAdapter adapter;
+
+    /**
+     * Generates a codeGenerator using the .properties the path points to.
+     *
+     * @param propertyLocation The location of the properties file to use
+     */
+    public CodeGenerator(final Path propertyLocation)
+            throws InvalidConfigurationException {
+
+        Properties properties = new Properties();
+        log.info("Trying to load properties from location [{}]",
+                 propertyLocation.toString());
+        try (InputStream in = new BufferedInputStream(
+                Files.newInputStream(propertyLocation))) {
+            properties.load(in);
+            Config.CONFIG.readInProperties(properties);
+            log.info("Successfully read in properties");
+        } catch (FileNotFoundException ex) {
+            log.warn("Did not find properties at location [{}]",
+                     propertyLocation.toString());
+            log.info("Using default properties");
+            Config.CONFIG.readInProperties(new Properties());
+        } catch (IOException ex) {
+            log.error("Something went wrong, exception message [{}]",
+                      ex.getMessage());
+            log.info("Using default properties");
+            Config.CONFIG.readInProperties(new Properties());
+        }
+
+        setUpAdapter();
+    }
+
+    private void setUpAdapter() {
+
+        log.info("Setting up adapter");
+        String adapterClass = Config.CONFIG.getAdpaterClass();
+        try {
+            Class<?> clazz = Class.forName(adapterClass);
+            if (ITemplateEngineAdapter.class.isAssignableFrom(clazz)) {
+                adapter = (ITemplateEngineAdapter) clazz.getConstructor()
+                        .newInstance();
+                adapter.initialize(Config.CONFIG.getLanguage());
+            } else {
+                log.error("[{}] does not implement ITemplateEngineAdapter, ",
+                          adapterClass);
+                throw new InvalidConfigurationException();
+            }
+        } catch (ClassNotFoundException e) {
+            log.error("[{}] was not found by the current classloader",
+                      adapterClass);
+            throw new InvalidConfigurationException(
+                    adapterClass + " was not " + "found");
+        } catch (InstantiationException e) {
+            log.error("Could not instantiate class [{}]", adapterClass);
+            throw new InvalidConfigurationException(
+                    "Could not instanciate " + adapterClass);
+        } catch (InvocationTargetException e) {
+            log.error("Constructor of class [{}] threw an exception with "
+                              + "message [{}]", adapterClass, e.getMessage());
+        } catch (NoSuchMethodException e) {
+            log.error("[{}] does not offer a parameterless default "
+                              + "constructor", adapterClass);
+            throw new InvalidConfigurationException(
+                    adapterClass + " does not offer a default constructor");
+        } catch (IllegalAccessException e) {
+            log.error("Could not access constructor of [{}], it might not be "
+                              + "public", adapterClass);
+            throw new InvalidConfigurationException(
+                    "Could not access constructor of " + adapterClass);
+        }
+    }
+
+    /**
+     * Reads in the XMI and produces code for the specified language.
+     *
+     * @param xmiFile  The file holding a valid XMI representation of an UML
+     *                 model
+     * @param language The language the generated code should be in
+     * @throws CodeGenerationException If the generation of code failed
+     */
+    public void generateCode(final Path xmiFile, final Language language)
+            throws CodeGenerationException {
+
+        Model model;
+        try {
+            model = SDMetricsUtil.parseXMI(xmiFile.toString());
+        } catch (Exception e) {
+            throw new CodeGenerationException(
+                    "Could not read XMI, nested exception is " + e.getClass()
+                            .getSimpleName() + ", message: " + e.getMessage());
+        }
+        IRestructurerMediator mediator = new IRestructurerMediator();
+        List<CommonElements> elementsList = mediator.restructure(model);
+        adapter.addToContext("elements", elementsList);
+        adapter.process("Class", Config.CONFIG.getOutputDirectory());
+    }
+}
