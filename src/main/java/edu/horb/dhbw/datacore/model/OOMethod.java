2f7a33ebc0111083eb34c25447e9fb87f15be823
Unknown <sebastian33.boehringer@gmail.com>
2020-03-06 17:21:12 +0100

    :new: Added an abstraction for oo-classes and other concepts
    The goal of this more abstract concept is to move away from the uml implementation and use this instead. The main benefit is the increased ease with which one can change the input processing chain of the codegenerator. Developers only need to mold their classes into those OO-based ones instead of using the uml-faithful implementation as a step in between

---
+++ src/main/java/edu/horb/dhbw/datacore/model/OOMethod.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2020 Sebastian Boehringer.
+ *  This file is part of the CodeGenerator.
+ *
+ *  CodeGenerator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ * CodeGenerator is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ * along with CodeGenerator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.horb.dhbw.datacore.model;
+
+import edu.horb.dhbw.datacore.uml.enums.VisibilityKind;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Collections;
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class OOMethod extends OOBase {
+    private Object logic;
+    private List<OOParameter> parameters = Collections.emptyList();
+    private VisibilityKind visbility;
+
+}
---
+++ src/main/java/edu/horb/dhbw/datacore/model/OOPackage.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2020 Sebastian Boehringer.
+ *  This file is part of the CodeGenerator.
+ *
+ *  CodeGenerator is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
+ * CodeGenerator is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ * along with CodeGenerator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.horb.dhbw.datacore.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Collections;
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class OOPackage extends OOBase{
+
+    private List<OOClass> classes = Collections.emptyList();
+
+    private List<OOInterface> interfaces = Collections.emptyList();
+
+}
