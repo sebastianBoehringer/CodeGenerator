0cc8087d87685865f9427ae6cc902a164dd9f059
Unknown <sebastian33.boehringer@gmail.com>
2020-03-12 15:39:24 +0100

    :wrench: Main: removed comments, made path to properties relative

--- src/main/java/edu/horb/dhbw/Main.java
+++ src/main/java/edu/horb/dhbw/Main.java
@@ -1,7 +1,5 @@
 package edu.horb.dhbw;
 
-import edu.horb.dhbw.templating.ITemplateEngineAdapter;
-import edu.horb.dhbw.templating.ThymeleafAdapter;
 import edu.horb.dhbw.util.Config;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,19 +16,8 @@ public final class Main {
             throws Exception {
 
         CodeGenerator gen = new CodeGenerator(
-                Path.of("C:/Users/SBG/IdeaProjects/CodeGenerator/src"
-                                + "/main/resources/default.properties"));
-        ITemplateEngineAdapter adapter = new ThymeleafAdapter();
+                Path.of("src/main/resources/default.properties"));
         gen.generateCode(Path.of("src/main/resources/xmi/uml.xmi"),
                          Config.CONFIG.getLanguage());
-
-        // mediator.restructure(model);
-        //Writer fileWriter = new
-        // FileWriter("./target/test
-        // .java");
-        //fileWriter.write(System
-        // .currentTimeMillis() + "\n");
-        //engine.process("test", context, fileWriter);
-        //System.out.println(engine.process("test", context));
     }
 }
