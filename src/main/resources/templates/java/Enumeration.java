1ddaad0361ffdd8645943180c0e1f841e5ca6ebd
Unknown <sebastian33.boehringer@gmail.com>
2020-03-13 12:30:00 +0100

    :wrench: EnumerationTemplate: Proper terminating of literals

--- src/main/resources/templates/java/Enumeration.java
+++ src/main/resources/templates/java/Enumeration.java
@@ -1,5 +1,6 @@
 [(${enum.getVisibility().toString()})] enum [(${enum.getName()})][# th:if="${enum.getSuperTypes().size()} gt 0"] extends [# th:each="type : ${enum.getSuperTypes()}"][(${type.getName()})],[/][/]{
-        [# th:each="literal: ${enum.getLiterals()}"][(${literal.toUpperCase()})],
+        [# th:each="literal, iterStat: ${enum.getLiterals()}"][(${literal.toUpperCase()})] [# th:unless="${iterStat.last}"],
+        [/][# th:if="${iterStat.last}"];[/]
         [/]
         [# th:each="attribute : ${enum.getFields()}"][# th:insert="Attribute"][/]
         [/]
