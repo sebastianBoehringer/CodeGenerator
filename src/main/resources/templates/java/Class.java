fe463c3d90c9840d8df1344abfa9e8501e6bd887
Unknown <sebastian33.boehringer@gmail.com>
2020-03-13 12:16:53 +0100

    :new: ClassTemplate: takes interfaces into account now

--- src/main/resources/templates/java/Class.java
+++ src/main/resources/templates/java/Class.java
@@ -1,4 +1,5 @@
-[(${class.getVisibility().toString()})] [# th:if="${class.isAbstract()}"]abstract [/]class [(${class.getName()})][# th:if="${class.getSuperTypes().size()} gt 0"] extends [# th:each="type,iterStat : ${class.getSuperTypes()}"][(${type.getName()})][# th:unless="${iterStat.last}"], [/][/][/] {
+[(${class.getVisibility().toString()})] [# th:if="${class.isAbstract()}"]abstract [/]class [(${class.getName()})][# th:if="${class.getSuperTypes().size()} gt 0"] extends [# th:each="type,iterStat : ${class.getSuperTypes()}"][(${type.getName()})][# th:unless="${iterStat.last}"], [/][/][/][# th:if="${class.getImplementedInterfaces().size()} gt 0"]
+        [# th:each="iface,iterStat : ${class.getImplementedInterfaces()}"][(${iface.getName()})][# th:unless="${iterStat.last}"],[/][/][/] {
 [# th:each="attribute,iterStat : ${class.getFields()}"]    [# th:insert="Attribute"][/][# th:unless="${iterStat.last}"]
 [/][/]
 
